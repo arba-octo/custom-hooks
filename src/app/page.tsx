4f175518ffb4cf9f20c558963338077ecf8d7e0f
Tatyana Arb <nevarus@yandex.ru>
2024-11-02 19:56:24 +0300

    ch-7 feat: добавлен компонент Title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Header from "@/components/Header";
-import Line from "@/components/Line";
+import Header from '@/components/Header';
+import Line from '@/components/Line';
+import Title from '@/components/Title';
 
 export default function Home() {
   return (
-   <div>
-     <Header/>
-       <Line/>
-   </div>
+    <div>
+      <Header/>
+      <Line/>
+      <Title/>
+    </div>
   );
 }
