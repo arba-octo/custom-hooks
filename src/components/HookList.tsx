2bc6d6a1369afbaefc4144166dd3caba8d7e2731
arba-octo <121129925+arba-octo@users.noreply.github.com>
2024-11-04 14:12:13 +0300

    ch-9 feat: добавлена картинка в компонент HookList (#9)

---
+++ src/components/HookList.tsx
@@ -0,0 +1,26 @@
+
+const HookList = () => {
+  return (
+    <div className="flex max-w-7xl mx-auto px-20 mt-3">
+      <h1>
+          Hooks
+      </h1>
+      <ul>
+        <li>useCookies</li>
+        <li>useDebounce</li>
+        <li>useForceUpdate</li>
+        <li>useHover</li>
+        <li>useIncrement</li>
+        <li>useIsMobile</li>
+        <li>useLocalStorage</li>
+        <li>useMeasure</li>
+        <li>useScript</li>
+        <li>useScrollFreeze</li>
+        <li>useTheme</li>
+        <li>useToggle</li>
+        <li>useWindowWidth</li>
+      </ul>
+    </div>
+  );
+};
+export default HookList;
