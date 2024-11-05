69debc4b46c519f2c4e6cfb52fbeaba747c28fe7
Tatyana Arb <nevarus@yandex.ru>
2024-11-05 14:41:11 +0300

    ch-10 feat: добавлена коллекция хуков и их вывод

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,33 @@
+import path from 'path';
+import fs from 'fs';
 import Header from '@/components/Header';
 import Line from '@/components/Line';
 import Title from '@/components/Title';
 import HookList from '@/components/HookList';
 
-export default function Home() {
+const getCustomHooks = () => {
+  const hooksPath = path.join(process.cwd(), 'collections');
+  const filenames = fs.readdirSync(hooksPath);
+
+  return filenames.reduce((acc, hookName: string) => {
+    const filePath = path.join(hooksPath, hookName);
+    const hookCode = fs.readFileSync(filePath, 'utf-8');
+
+    return acc.concat({ hookName, hookCode });
+  }, [] as { hookName: string, hookCode: string }[]);
+};
+
+function Home() {
+  const hooksCollectionData = getCustomHooks();
+
   return (
     <div>
       <Header/>
       <Line/>
       <Title/>
-      <HookList />
+      <HookList hooksCollection={hooksCollectionData} />
     </div>
   );
 }
+
+export default Home;
