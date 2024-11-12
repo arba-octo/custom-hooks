53fe291c6d84a4de0fb28ccb94ffeb14dffdb46c
Tatyana Arb <nevarus@yandex.ru>
2024-11-12 10:50:09 +0300

    ch-14 feat: завершена верстка Footer

--- src/components/FooterLinksList.tsx
+++ src/components/FooterLinksList.tsx
@@ -1,17 +1,25 @@
-import FOOTERLINKSDATAS from "@/datas/footerLinksDatas";
+import FOOTERLINKSDATAS from '@/datas/footerLinksDatas';
 
-
-const FooterLinksList = (FOOTERLINKSDATAS) => {
+const FooterLinksList = () => {
   return (
-      <ul>
-          FOOTERLINKSDATAS.map((item) => {
+    <div className="grid grid-cols-3 justify-between leading-[25px]">
+      {FOOTERLINKSDATAS.map((itemList) => {
+        return (
+          <ul key={itemList.id}>
+            <h2>{itemList.titleData}</h2>
+            {itemList.linksListData.map((itemLink) => {
               return (
-                  <li className="text-[13px] leading-[16px] text-[#666666]">
-                    <a className="" href={ item.linksListData.linkData }>{ item.linksListData.nameData}</a>
-                    </li>
-              )
-            }
-      </ul>
+                <li className="text-[13px] leading-[16px] text-[#666666] leading-[20px]" key={itemLink.id}>
+                  <a className="" href={itemLink.linkData}>{itemLink.nameData}</a>
+                </li>
+              );
+            })}
+          </ul>
+        );
+      })}
+    </div>
+
+
   );
 };
 export default FooterLinksList;
