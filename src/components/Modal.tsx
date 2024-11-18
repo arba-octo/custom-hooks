cb3093f3fe071047ee147c1f177c6e00ba201cf5
arba-octo <121129925+arba-octo@users.noreply.github.com>
2024-11-18 21:06:25 +0300

    сh-16: настройка провайдера (#16)
    * ch-15 feat: замена favicon
    
    * ch-16 feat: настройка провайдера

---
+++ src/components/Modal.tsx
@@ -0,0 +1,13 @@
+function Modal({ hooksCollection }) {
+  return (
+    <div className="fixed top-0 right-0 left-0 bottom-0 bg-amber-100">
+      <div className="flex justify-center mt-10">
+        <div className='min-w-[800px] bg-orange-400'>
+          {'hookCodeModal'}
+        </div>
+      </div>
+    </div>
+
+  );
+}
+export default Modal;
